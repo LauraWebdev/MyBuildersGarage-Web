b3b6554d1a0f147310afa61d4bb1ef88780d5f13
Laura Heimann <hello@laura.media>
2021-05-19 21:33:52 +0200

    added frontend routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Store from '../store/index';
 import Index from '../views/Index.vue';
+import Search from '../views/Search.vue';
+import AllChannels from '../views/Channel/AllChannels.vue';
+import ChannelDetail from '../views/Channel/ChannelDetail.vue';
+import UserDetail from '../views/User/UserDetail.vue';
+import GameDetail from '../views/Game/GameDetail.vue';
+import PlaylistDetail from '../views/Playlist/PlaylistDetail.vue';
+import Legal from '../views/Legal.vue';
 
 Vue.use(VueRouter);
 
@@ -11,6 +18,55 @@ const routes = [{
     component: Index,
     meta: {
         title: 'Discover & share game projects',
+    }
+}, {
+    path: '/search/:query',
+    name: 'Search',
+    component: Search,
+    meta: {
+        title: 'Search results',
+    }
+}, {
+    path: '/channel/all',
+    name: 'AllChannels',
+    component: AllChannels,
+    meta: {
+        title: 'Channels',
+    }
+}, {
+    path: '/channel/:id',
+    name: 'ChannelDetail',
+    component: ChannelDetail,
+    meta: {
+        title: 'Channel',
+    }
+}, {
+    path: '/user/:id',
+    name: 'UserDetail',
+    component: UserDetail,
+    meta: {
+        title: 'User profile',
+    }
+}, {
+    path: '/game/:id',
+    name: 'GameDetail',
+    component: GameDetail,
+    meta: {
+        title: 'Game page',
+    }
+}, {
+    path: '/playlist/:id',
+    name: 'PlaylistDetail',
+    component: PlaylistDetail,
+    meta: {
+        title: 'Playlist',
+    }
+}, {
+    path: '/legal',
+    name: 'Legal',
+    component: Legal,
+    meta: {
+        title: 'Legal information',
     }
 }];
 
