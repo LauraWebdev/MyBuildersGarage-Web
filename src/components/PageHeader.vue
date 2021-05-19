aee95eba5572a620080cf4599c5333b88f9e95db
Laura Heimann <hello@laura.media>
2021-05-19 22:47:01 +0200

    switch searchbox placeholder with links and change display for logged in users

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,15 +1,18 @@
 <template>
     <header>
-        <img src="@/assets/img/logoLight.svg" class="logo" />
+        <router-link :to="{name: 'Index'}">
+            <img src="@/assets/img/logoLight.svg" class="logo" />
+        </router-link>
 
-        <div class="search">
-            SEARCH BOX
-        </div>
+        <nav>
+            <router-link :to="{name: 'Index'}">Home</router-link>
+            <router-link :to="{name: 'Search', params: {query: 'a'}}">Search</router-link>
+        </nav>
 
-        <div class="actions" v-if="$store.state.userToken !== null">
-            <router-link to="/" class="avatar" :style="'background: url(' + $store.state.userData.avatarFileName + ');'"></router-link>
+        <div class="actions" v-if="$store.state.userToken !== null && $store.state.userToken !== undefined">
+            <router-link :to="{name: 'UserDetail', params: {id: $store.state.userData.id}}" class="avatar" :style="'background-image: url(' + $store.state.userData.avatarFileName + ');'"></router-link>
         </div>
-        <div class="actions" v-if="$store.state.userToken === null">
+        <div class="actions" v-if="$store.state.userToken === null || $store.state.userToken === undefined">
             <Button to="/auth/register" filled>Join now!</Button>
             <Button to="/auth/login" class="button">Login</Button>
         </div>
@@ -17,13 +20,15 @@
 </template>
 
 <script>
+    import { mapGetters } from "vuex";
     import Button from '@/components/Button';
 
     export default {
         name: 'PageHeader',
         components: {
             Button
-        }
+        },
+
     }
 </script>
 
@@ -34,9 +39,23 @@
         display: grid;
         grid-template-columns: auto 1fr auto;
         grid-gap: 15px;
+        align-items: center;
 
         & .logo {
             height: 35px;
+            margin: 0px 15px;
+        }
+
+        & .actions {
+            display: flex;
+            & .avatar {
+                background: rgba(0,0,0,0.1);
+                display: block;
+                width: 36px;
+                height: 36px;
+                margin: 10px;
+                border-radius: 50px;
+            }
         }
     }
 </style>
