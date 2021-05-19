7cfe8dc64e9a38aeef1565e6c9e76245a8ed63b0
Laura Heimann <hello@laura.media>
2021-05-19 21:36:35 +0200

    add legal link and disclaimer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,5 +1,8 @@
 <template>
-    <footer>Disclaimer</footer>
+    <footer>
+        <p>MyGarage.games is a free platform by the community and in no way affiliated with Nintendo. Game Builder Garage is a trademark by Nintendo.</p>
+        <router-link :to="{name: 'Legal'}">Legal information</router-link>
+    </footer>
 </template>
 
 <script>
