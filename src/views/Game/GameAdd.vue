703007eb5ffd434bcd37e066c3669ec39ba9440c
Laura Heimann <hello@laura.media>
2021-05-20 22:07:11 +0200

    add game add scaffolding view

---
+++ src/views/Game/GameAdd.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page-wrapper page-gameadd">
+        Game Add
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GameAdd',
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
