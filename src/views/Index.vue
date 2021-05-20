8af240da81978840582b9de656c266a68501b1e3
Laura Heimann <hello@laura.media>
2021-05-20 21:10:40 +0200

    remove login from index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,6 @@
 <template>
-    <div class="home">
-        <div v-if="$store.state.userToken !== null && $store.state.userToken !== undefined">
-            <b>UserData</b><br />
-            UserToken: {{ $store.state.userToken }}<br />
-            UserName: {{ $store.state.userData.username }}<br /><br />
-            <button v-on:click="logout()">Logout</button>
-        </div>
-        <hr />
-        <strong v-if="errorMessage != ''">{{ errorMessage }}</strong>
-        <hr />
-        Username: <input type="text" v-model="userName" /><br />
-        Password: <input type="password" v-model="userPass" /><br /><br />
-        <button v-on:click="login()">Login</button>
+    <div class="page-index">
+        Welcome Message <br /><br />Newest Games
     </div>
 </template>
 
@@ -25,41 +14,12 @@
         data: function() {
             return {
                 apiRef: null,
-                userName: "",
-                userPass: "",
-                errorMessage: "",
             }
         },
         created: function() {
             this.$data.apiRef = new MGGApi(true);
         },
         methods: {
-            async login() {
-                this.$data.errorMessage = "";
-
-                try {
-                    const loginResponse = await this.$data.apiRef.authLogin(this.$data.userName, this.$data.userPass);
-                    this.$store.dispatch('refreshUser', loginResponse);
-                } catch(error) {
-                    switch(error.name) {
-                        case "UserNotFoundException":
-                            // Wrong username
-                            this.$data.errorMessage = "This user does not exist.";
-                            break;
-                        case "AuthenticationWrongException":
-                            // Wrong Password
-                            this.$data.errorMessage = "The password is incorrect.";
-                            break;
-                    }
-                }
-            },
-            logout() {
-                this.$store.dispatch('refreshUser', {
-                    userData: null,
-                    userRoles: null,
-                    userToken: null
-                });
-            }
         }
     }
 </script>
