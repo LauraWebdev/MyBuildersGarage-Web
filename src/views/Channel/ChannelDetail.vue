e383eebc88413464e0a70fed20880b671f0516a0
Laura Heimann <hello@laura.media>
2021-05-22 19:16:55 +0200

    added channeloverview and channeldetail pages

--- src/views/Channel/ChannelDetail.vue
+++ src/views/Channel/ChannelDetail.vue
@@ -1,11 +1,61 @@
 <template>
-    <div class="page-wrapper page-channeldetail">
-        Channel Detail
+    <div class="page-channelOverview">
+        <div class="page-centered page-header" v-if="channelDetail != null">
+            <div class="page-wrapper">
+                <h1>{{ channelDetail.title }}</h1>
+                <p>{{ channelDetail.description }}</p>
+            </div>
+        </div>
+
+        <div class="page-centered page-channelList" v-if="channelDetail != null">
+            <div class="page-wrapper">
+                <GameList>
+                    <GameItem v-for="game in channelDetail.games" v-bind:key="game.id" v-bind="game"></GameItem>
+                </GameList>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import MGGApi from '../../modules/api';
+
+    import GameList from '@/components/Game/GameList';
+    import GameItem from '@/components/Game/GameItem';
+
     export default {
         name: 'ChannelDetail',
+        components: {
+            GameList,
+            GameItem,
+        },
+        data: function() {
+            return {
+                apiRef: null,
+                channelDetail: null,
+            }
+        },
+        created: function() {
+            this.$data.apiRef = new MGGApi(true);
+        },
+        mounted: function() {
+            this.fetchChannelDetail(this.$router.currentRoute.params.id);
+        },
+        methods: {
+            fetchChannelDetail: async function(channelID) {
+                try {
+                    this.$data.channelDetail = await this.$data.apiRef.getChannelDetail(channelID);
+                    console.log(this.$data.channelDetail);
+                } catch(error) {
+                    console.error(error);
+                }
+            }
+        }
     }
 </script>
+
+<style lang="less" scoped>
+    .page-channelList {
+        margin: 50px 0px;
+    }
+</style>
--- src/views/Channel/ChannelOverview.vue
+++ src/views/Channel/ChannelOverview.vue
@@ -1,11 +1,60 @@
 <template>
-    <div class="page-wrapper page-channelOverview">
-        All Channels
+    <div class="page-channelOverview">
+        <div class="page-centered page-header">
+            <div class="page-wrapper">
+                <h1>Channel Overview</h1>
+                <p>Discover new games by a specific topic, style or concept.</p>
+            </div>
+        </div>
+
+        <div class="page-centered page-channelList">
+            <div class="page-wrapper">
+                <ChannelList>
+                    <ChannelItem v-for="channel in channels" v-bind:key="channel.id" v-bind="channel"></ChannelItem>
+                </ChannelList>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import MGGApi from '../../modules/api';
+
+    import ChannelList from '@/components/Channel/ChannelList';
+    import ChannelItem from '@/components/Channel/ChannelItem';
+
     export default {
         name: 'ChannelOverview',
+        components: {
+            ChannelList,
+            ChannelItem,
+        },
+        data: function() {
+            return {
+                apiRef: null,
+                channels: [],
+            }
+        },
+        created: function() {
+            this.$data.apiRef = new MGGApi(true);
+        },
+        mounted: function() {
+            this.fetchChannels();
+        },
+        methods: {
+            fetchChannels: async function() {
+                try {
+                    this.$data.channels = await this.$data.apiRef.getAllChannels();
+                } catch(error) {
+                    console.error(error);
+                }
+            }
+        }
     }
 </script>
+
+<style lang="less" scoped>
+    .page-channelList {
+        margin: 50px 0px;
+    }
+</style>
