448ee088de69df9de4a8a9f9cc29938f9ab00456
Laura Heimann <hello@laura.media>
2021-05-23 21:54:14 +0200

    add stage 1 of game detail page

--- src/views/Game/GameDetail.vue
+++ src/views/Game/GameDetail.vue
@@ -1,11 +1,324 @@
 <template>
-    <div class="page-wrapper page-gamedetail">
-        Game Detail
+    <div class="page-gamedetail">
+        <div class="game-detail" v-if="!apiLoading">
+            <div class="game-header" :style="`background-image: url('${gameDetail.coverFileName}')`" v-on:click="openTrailer()" v-if="gameDetail.youtubeID != ''">
+                <div class="header-shade">
+                    <span class="mdi mdi-play-circle-outline"></span>
+                </div>
+            </div>
+
+            <div class="page-centered">
+                <div class="page-wrapper page-thirdssplit">
+                    <div class="game-meta">
+                        <h1>{{ gameDetail.title }}</h1>
+                        <router-link :to="{ name: 'UserDetail', params: { id: gameDetail.user.id } }" class="user">
+                            <div class="avatar" :style="`background-image: url('${ gameDetail.user.avatarFileName }')`"></div>
+                            <div class="username">{{ gameDetail.user.username }}</div>
+                        </router-link>
+                        <div class="gameID">
+                            <div class="ingameID">{{ gameDetail.ingameID }}</div>
+                            <div class="addToPlaylist"><span class="mdi mdi-heart-outline"></span></div>
+                        </div>
+                        <div class="description" v-if="gameDetail.description != ''">
+                            {{ gameDetail.description }}
+                        </div>
+                        <div class="meta-info">
+                            <div class="item">
+                                <div>Created</div>
+                                <div>{{ createdFormatted }}</div>
+                            </div>
+                            <div class="item" v-if="gameDetail.updatedAt != gameDetail.createdAt">
+                                <div>Updated</div>
+                                <div>{{ updatedFormatted }}</div>
+                            </div>
+                        </div>
+                        <div class="channels" v-if="gameDetail.channels.length > 0">
+                            <router-link :to="{ name: 'ChannelDetail', params: { id: channel.id } }" class="item" v-for="channel in gameDetail.channels" v-bind:key="channel.id">{{ channel.title }}</router-link>
+                        </div>
+                    </div>
+                    <div class="game-screenshots">
+                        SCREENSHOTS
+                    </div>
+                </div>
+            </div>
+
+            <div class="trailer-overlay" v-if="trailerOverlayOpen">
+                <div class="video-embed">
+                    <div class="video-16by9">
+                        <iframe width="100%" height="100%" :src="`https://www.youtube-nocookie.com/embed/${gameDetail.youtubeID}`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div v-on:click="closeTrailer()" class="close-button">Close</div>
+            </div>
+        </div>
+        <div class="game-loading" v-if="apiLoading">
+            <LoadingCircle />
+        </div>
     </div>
 </template>
 
 <script>
+    import MGGApi from '../../modules/api';
+
+    import LoadingCircle from '@/components/General/LoadingCircle';
+
     export default {
         name: 'GameDetail',
+        data: function() {
+            return {
+                apiRef: null,
+                apiLoading: true,
+                trailerOverlayOpen: false,
+                gameDetail: null,
+                createdFormatted: "",
+                updatedFormatted: "",
+            }
+        },
+        components: {
+            LoadingCircle,
+        },
+        created: function() {
+            this.$data.apiRef = new MGGApi(true);
+        },
+        mounted: function() {
+            this.loadGame();
+        },
+        methods: {
+            loadGame: async function() {
+                this.$data.apiLoading = true;
+
+                try {
+                    this.$data.gameDetail = await this.$data.apiRef.getGameDetail(this.$router.currentRoute.params.id);
+                    this.$data.apiLoading = false;
+
+                    document.title = `${this.$data.gameDetail.title} by ${this.$data.gameDetail.user.username} ~ MyGarage.games`;
+
+                    this.$data.createdFormatted = new Date(Date.parse(this.$data.gameDetail.createdAt)).toLocaleDateString("en-US");
+                    this.$data.updatedFormatted = new Date(Date.parse(this.$data.gameDetail.updatedAt)).toLocaleDateString("en-US");
+                } catch(error) {
+                    console.error(error);
+                    this.$data.apiLoading = false;
+                }
+            },
+            openTrailer: function() {
+                // TODO: Show Overlay with YouTube Embed
+                this.$data.trailerOverlayOpen = true;
+            },
+            closeTrailer: function() {
+                this.$data.trailerOverlayOpen = false;
+            },
+            addToPlaylist: function() {
+
+            }
+        }
     }
 </script>
+
+<style lang="less" scoped>
+    .game-loading {
+        padding: 150px 0px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    & .game-header {
+        width: 100%;
+        height: 300px;
+        background-position: center;
+        background-size: cover;
+        position: relative;
+    }
+    & .header-shade {
+        background: linear-gradient(180deg, rgba(0,0,0,0.2), rgba(0,0,0,0.6));
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        backdrop-filter: blur(5px);
+        transition: 0.2s ease-in-out all;
+
+        & .mdi {
+            font-size: 64px;
+            color: #fff;
+            transition: 0.2s ease-in-out opacity;
+        }
+        &:hover {
+            backdrop-filter: blur(0px);
+
+            & .mdi {
+                opacity: 0.6;
+            }
+        }
+    }
+    & .page-thirdssplit {
+        display: grid;
+        grid-template-columns: minmax(350px, 1fr) 2fr;
+        grid-gap: 25px;
+        padding: 50px 0px;
+    }
+    & .trailer-overlay {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background: rgba(0,0,0,0.6);
+        z-index: 100;
+
+        & .video-embed {
+            max-width: 960px;
+            width: 80%;
+
+            & .video-16by9 {
+                width: 100%;
+                padding-top: 56.25%;
+                position:  relative;
+
+                & iframe {
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                }
+            }
+        }
+        & .close-button {
+            color: #fff;
+            border: 2px solid #fff;
+            border-radius: 5px;
+            font-weight: bold;
+            padding: 7px 25px;
+            text-transform: uppercase;
+            letter-spacing: 0.1em;
+            font-size: 12px;
+            transition: 0.2s ease-in-out background, 0.2s ease-in-out color;
+            font-family: 'Poppins', sans-serif;
+            margin-top: 25px;
+
+            &:hover {
+                color: #000;
+                background: #fff;
+                cursor: pointer;
+            }
+            &:focus {
+                outline: none;
+            }
+        }
+    }
+    & .game-meta {
+        & h1 {
+            font-weight: bold;
+            font-size: 32px;
+            margin: 0;
+        }
+        & .gameID {
+            display: grid;
+            grid-template-columns: 1fr auto;
+            grid-gap: 15px;
+            margin-bottom: 15px;
+
+            & .ingameID {
+                background: #25baff44;
+                color: #1EA0DE;
+                font-size: 18px;
+                display: flex;
+                align-items: center;
+                font-weight: bold;
+                padding: 8px 20px;
+                border-radius: 100px;
+            }
+            & .addToPlaylist {
+                background: #25B9FF;
+                border: 2px solid #1a8fc5;
+                color: #fff;
+                border-radius: 50%;
+                width: 50px;
+                height: 50px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 20px;
+                transition: 0.2s ease-in-out background;
+
+                &:hover {
+                    background: #1a8fc5;
+                    cursor: pointer;
+                }
+            }
+        }
+        & .user {
+            display: grid;
+            grid-template-columns: auto 1fr;
+            grid-gap: 10px;
+            align-content: center;
+            transition: 0.2s ease-in-out opacity;
+            text-decoration: none;
+            margin-bottom: 25px;
+
+            & .avatar {
+                width: 24px;
+                height: 24px;
+                background: rgba(0,0,0,0.1);
+                background-position: center;
+                background-size: cover;
+                border-radius: 50%;
+            }
+            & .username {
+                overflow: hidden;
+            }
+
+            &:hover {
+                opacity: 0.4;
+            }
+        }
+        & .description {
+            line-height: 1.5em;
+            margin-bottom: 25px;
+        }
+        & .meta-info {
+            display: grid;
+            grid-gap: 10px;
+            margin-bottom: 25px;
+
+            & .item {
+                display: grid;
+                grid-template-columns: 1fr 2fr;
+                grid-gap: 15px;
+
+                & div:nth-child(1) {
+                    opacity: 0.6;
+                }
+            }
+        }
+        & .channels {
+            display: flex;
+
+            & .item {
+                padding: 4px 12px;
+                background: #25baff44;
+                color: #1EA0DE;
+                border-radius: 100px;
+                margin-right: 5px;
+                transition: 0.2s ease-in-out all;
+                text-decoration: none;
+
+                &:hover {
+                    background: #1EA0DE;
+                    color: #fff;
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+</style>
