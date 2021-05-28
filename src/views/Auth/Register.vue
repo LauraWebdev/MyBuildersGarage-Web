ca005f1e3e3370d27976a2687a1ebfb009d864c5
Laura Heimann <hello@laura.media>
2021-05-28 21:26:02 +0200

    swapped errorbox with snackbar

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -2,11 +2,9 @@
     <div class="page-centered page-register">
         <div class="register-box">
             <div class="box-header">Create a new account</div>
-            <div class="box-error" v-if="errorMessage">
-                {{ errorMessage }}
-            </div>
             <div class="box-content">
                 <input class="input" type="text" v-model="userName" placeholder="Username" />
+                <span>Allowed characters: a-z, A-Z, 0-9, hyphens, underscores</span>
                 <input class="input" type="password" v-model="userPass" placeholder="Password" />
                 <input class="input" type="password" v-model="userPass2" placeholder="Password (again)" />
                 <input class="input" type="email" v-model="userMail" placeholder="Email" />
@@ -31,7 +29,6 @@
                 userPass: "",
                 userPass2: "",
                 userMail: "",
-                errorMessage: "",
             }
         },
         created: function() {
@@ -39,38 +36,73 @@
         },
         methods: {
             register: async function() {
-                this.$data.errorMessage = "";
-
                 if(this.$data.userName == "" || this.$data.userPass == "" || this.$data.userPass2 == "" || this.$data.userMail == "") {
-                    this.$data.errorMessage = "All fields have to be filled out";
+                    this.$root.$emit('addSnackbar', {
+                        type: "error",
+                        icon: "key",
+                        text: "All fields must be filled out.",
+                        stay: false,
+                    });
                     return;
                 }
 
                 if(!this.$data.userMail.includes("@")) {
-                    this.$data.errorMessage = "Please use a valid email";
+                    this.$root.$emit('addSnackbar', {
+                        type: "error",
+                        icon: "key",
+                        text: "Please use a valid email adress.",
+                        stay: false,
+                    });
                     return;
                 }
 
                 if(this.$data.userPass != this.$data.userPass2) {
-                    this.$data.errorMessage = "The passwords are not the same.";
+                    this.$root.$emit('addSnackbar', {
+                        type: "error",
+                        icon: "key",
+                        text: "The entered passwords are not identical.",
+                        stay: false,
+                    });
                     return;
                 }
 
                 try {
                     const registerResponse = await this.$data.apiRef.authRegister(this.$data.userName, this.$data.userPass, this.$data.userMail);
-                    console.log(registerResponse);
+                    
+                    this.$root.$emit('addSnackbar', {
+                        type: "success",
+                        icon: "key",
+                        text: "Successfully created a new account.",
+                        stay: false,
+                    });
                     this.$router.push({ name: 'Login' });
                 } catch(error) {
                     console.error(error);
 
                     switch(error.name) {
-                        case "UserNotFoundException":
-                            // Wrong username
-                            this.$data.errorMessage = "This user does not exist.";
+                        default:
+                            this.$root.$emit('addSnackbar', {
+                                type: "error",
+                                icon: "key",
+                                text: "Could not create account due to a server error. Please try again later.",
+                                stay: false,
+                            });
                             break;
-                        case "AuthenticationWrongException":
-                            // Wrong Password
-                            this.$data.errorMessage = "The password is incorrect.";
+                        case "UsernameInvalidException":
+                            this.$root.$emit('addSnackbar', {
+                                type: "error",
+                                icon: "key",
+                                text: "This username contains forbidden characters. Allowed characters are a-z, A-Z, 0-9, hyphens and underscores.",
+                                stay: false,
+                            });
+                            break;
+                        case "UsernameEmailConflictException":
+                            this.$root.$emit('addSnackbar', {
+                                type: "error",
+                                icon: "key",
+                                text: "This username or email is already in use!",
+                                stay: false,
+                            });
                             break;
                     }
                 }
@@ -101,13 +133,6 @@
                 text-transform: uppercase;
                 letter-spacing: 0.15em;
             }
-            & .box-error {
-                background: #fd084d;
-                color: #fff;
-                font-weight: bold;
-                text-align: center;
-                padding: 15px;
-            }
             & .box-content {
                 padding: 20px;
                 display: grid;
@@ -116,6 +141,12 @@
                 & button {
                     justify-self: right;
                 }
+            }
+
+            & span {
+                margin-top: -8px;
+                margin-bottom: 8px;
+                font-size: 10px;
             }
         }
     }
