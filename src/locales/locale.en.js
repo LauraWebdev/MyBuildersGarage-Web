b2dda89df4ccd44005a932021aa30feb9d1ef4ec
Laura Heimann <hello@laura.media>
2021-06-11 00:29:35 +0200

    added german locale

--- src/locales/locale.en.js
+++ src/locales/locale.en.js
@@ -65,7 +65,9 @@ const messages = {
     },
     "index": {
         "welcome": "Welcome to the MyGarage.games BETA!<br />Please report bugs and feedback in the Discord, thank you!",
-        "support": "Hey there, if you'd like to support me or help us fund our servers, please consider donating to the PayPal moneypool or buy one of our games on itch.io! We want to keep MyGarage.games free off advertisements and any \"plus membership\" system. <i>&dash; Laura</i>"
+        "support": "Hey there, if you'd like to support me or help us fund our servers, please consider donating to the PayPal moneypool or buy one of our games on itch.io! We want to keep MyGarage.games free off advertisements and any \"plus membership\" system. <i>&dash; Laura</i>",
+        "newestHeader": "Newest Games",
+        "popularHeader": "Popular Games"
     },
     "find": {
         "header": "Find",
