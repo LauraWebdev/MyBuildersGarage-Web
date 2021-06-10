543f8f7a01f170058242a15d4a9df4c37f9094d4
Laura Heimann <hello@laura.media>
2021-06-10 16:18:48 +0200

    removed unneeded router meta titles

--- src/router/index.js
+++ src/router/index.js
@@ -26,122 +26,71 @@ const routes = [{
     path: '/',
     name: 'Index',
     component: Index,
-    meta: {
-        title: 'Discover & share game projects',
-    }
 }, {
     path: '/search/:query?',
     name: 'Search',
     component: Search,
-    meta: {
-        title: 'Search results',
-    }
 }, {
     path: '/channel/overview',
     name: 'ChannelOverview',
     component: ChannelOverview,
-    meta: {
-        title: 'Channels',
-    }
 }, {
     path: '/channel/:id',
     name: 'ChannelDetail',
     component: ChannelDetail,
-    meta: {
-        title: 'Channel',
-    }
 }, {
     path: '/user/:id',
     name: 'UserDetail',
     component: UserDetail,
-    meta: {
-        title: 'User profile',
-    }
 }, {
     path: '/user/:id/edit',
     name: 'UserEdit',
     component: UserEdit,
-    meta: {
-        title: 'Editing a user',
-    }
 }, {
     path: '/game/add',
     name: 'GameAdd',
     component: GameAdd,
-    meta: {
-        title: 'Add a new Game',
-    }
 }, {
     path: '/game/:id/edit',
     name: 'GameEdit',
     component: GameEdit,
-    meta: {
-        title: 'Editing a game',
-    }
 }, {
     path: '/game/:id/delete',
     name: 'GameDelete',
     component: GameDelete,
-    meta: {
-        title: 'Removing a game',
-    }
 }, {
     path: '/game/:id',
     name: 'GameDetail',
     component: GameDetail,
-    meta: {
-        title: 'Game page',
-    }
 }, {
     path: '/playlist/:id',
     name: 'PlaylistDetail',
     component: PlaylistDetail,
-    meta: {
-        title: 'Playlist',
-    }
 }, {
     path: '/legal',
     name: 'Legal',
     component: Legal,
-    meta: {
-        title: 'Legal information',
-    }
 }, {
     path: '/talk',
     name: 'Talk',
     component: Talk,
-    meta: {
-        title: 'Other GBG projects',
-    }
 }, {
     path: '/auth/login',
     name: 'Login',
     component: Login,
-    meta: {
-        title: 'Login',
-    }
 }, {
     path: '/auth/register',
     name: 'Register',
     component: Register,
-    meta: {
-        title: 'Create a new acount',
-    }
 }, {
     path: '/auth/logout',
     name: 'Logout',
     component: Logout,
-    meta: {
-        title: 'Logout',
-    }
 }, {
     path: '/auth/callback/discord',
     name: 'OauthCallbackDiscord',
     component: OauthCallback,
     props: { method: 'discord' },
-    meta: {
-        title: 'Login',
-    }
 }];
 
 const router = new VueRouter({
