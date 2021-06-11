cbf3ee50b97c8734d41bfd3d3547d4b232ce14d5
Laura Heimann <hello@laura.media>
2021-06-11 17:38:31 +0200

    activated dutch locale

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,10 +1,12 @@
 import messagesEN from './locale.en';
 import messagesDE from './locale.de';
+import messagesNL from './locale.nl';
 import messagesFR from './locale.fr';
 
 const languages = {
     "en": messagesEN,
     "de": messagesDE,
+    "nl": messagesNL,
     "fr": messagesFR
 };
 
