5192f20dcf0f5066e87d7ab6afa57086c5a55cf8
Laura Heimann <hello@laura.media>
2021-06-11 05:01:54 +0200

    enable french locale

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,9 +1,11 @@
 import messagesEN from './locale.en';
 import messagesDE from './locale.de';
+import messagesFR from './locale.fr';
 
 const languages = {
     "en": messagesEN,
-    "de": messagesDE
+    "de": messagesDE,
+    "fr": messagesFR
 };
 
 export default languages;
