7bc84b74f712387df6faaa0188081cbe907cc0ae
Laura Heimann <hello@laura.media>
2021-06-14 17:43:44 +0200

    changed comments ordering

--- src/components/Comments/CommentList.vue
+++ src/components/Comments/CommentList.vue
@@ -13,14 +13,8 @@
 <style lang="less" scoped>
     & .comment-list {
         display: grid;
-        grid-template-columns: 1fr 1fr;
+        grid-template-columns: 1fr;
         grid-gap: 25px;
         align-self: flex-start;
-    }
-
-    @media screen and (max-width: 800px) {
-        .comment-list {
-            grid-template-columns: 1fr;
-        }
     }
 </style>
