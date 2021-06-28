42dc40afb8b84afecf5a359f00deef92c0d121b0
Laura Heimann <hello@laura.media>
2021-06-28 19:18:04 +0200

    added korean translation, activated japanese translation

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,12 +2,16 @@ import messagesEN from './locale.en';
 import messagesDE from './locale.de';
 import messagesNL from './locale.nl';
 import messagesFR from './locale.fr';
+import messagesJA from './locale.ja';
+import messagesKO from './locale.ko';
 
 const languages = {
     "en": messagesEN,
     "de": messagesDE,
     "nl": messagesNL,
-    "fr": messagesFR
+    "fr": messagesFR,,
+    "ja": messagesJA
+    "ko": messagesKO
 };
 
 export default languages;
