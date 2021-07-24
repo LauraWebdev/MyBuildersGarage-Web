c03f03e519c69146ee140b483e77a6ff73285d76
Raul Solano <eyegraphic@eyegraphic.net>
2021-07-24 19:58:27 +0200

    Create locale.es.js
    Spanish translation

---
+++ src/locales/locale.es.js
@@ -0,0 +1,343 @@
+const messages = {
+    "header": {
+        "nav": {
+            "discover": "Descubre",
+            "channels": "Canales",
+            "find": "Buscar",
+            "talk": "Debate"
+        },
+        "actions": {
+            "join": "Únete ahora!",
+            "login": "Ingresar"
+        }
+    },
+    "login": {
+        "header": "Ingreso",
+        "banned": {
+            "message": "Tu estas baneado. Si tu crees que es un error, puedes enviarnos una apelación del baneo.",
+            "appealLinkText": "Formulario de Apelación del baneo"
+        },
+        "form": {
+            "usernamePlaceholder": "Nombre de usuario",
+            "passwordPlaceholder": "Contraseña",
+            "loginButton": "Ingresar"
+        },
+        "snackbar": {
+            "welcomeBack": "Bienvenido de nuevo, {username}",
+            "error": {
+                "fillOut": "Todos los campos deben rellenarse.",
+                "serverError": "Ha habido un error en el servidor ingresando. Por favor, inténtalo más tarde.",
+                "userDoesntExist": "Este usuario no existe.",
+                "passwordWrong": "La contraseña es incorrecta.",
+                "banned": "Tu cuenta ha sido baneada. Motivo: {reason}"
+            }
+        }
+    },
+    "logout": {
+        "header": "Hasta pronto",
+        "text": "Has salido del sistema correctamente!<br />Gracias por visitar MyGarage.games!",
+        "snackbar": {
+            "successful": "Has salido del sistema correctamente."
+        }
+    },
+    "oauthDiscordCallback": {
+        "header": "Ingresar con Discord",
+    },
+    "register": {
+        "header": "Crear una nueva cuenta",
+        "form": {
+            "usernamePlaceholder": "Nombre de usuario",
+            "usernameRequirements": "Caracteres permitidos: a-z, A-Z, 0-9, guiones, guiones bajos",
+            "passwordPlaceholder": "Contraseña",
+            "passwordAgainPlaceholder": "Contraseña (Repetir)",
+            "emailPlaceholder": "Email",
+            "registerButton": "Registro"
+        },
+        "snackbar": {
+            "success": "Se ha creado la nueva cuenta correctamente.",
+            "error": {
+                "emailInvalid": "Por favor, utiliza una cuenta de email válida.",
+                "passwordNotEqual": "Las contraseñas introducidas no coinciden.",
+                "registerServerError": "No se ha podido crear la cuenta debido a un error del servidor. Por favor inténtalo más tarde.",
+                "inUse": "El nombre de usuario o email estan ya utilizándose!"
+            }
+        }
+    },
+    "index": {
+        "welcome": "Bienvenido a MyGarage.games BETA!<br />Por favor, reporta errores y comentarios en el canal de Discord, ¡gracias!",
+        "support": "Ey, si quieres apoyarme o ayudarnos finanziando nuestros servidores, por favor, considera donar a la cuenta PayPal o compra uno de nuestros juegos en itch.io! Queremos mantener MyGarage.games libre de anuncios y cualquier tipo de suscripción. <i>&dash; Laura</i>",
+        "newestHeader": "Juegos nuevos",
+        "hotThisWeekHeader": "Tendencia esta semana",
+        "randomHeader": "Aleatorios",
+        "popularHeader": "Juegos populares",
+        "previousPage": "Página anterior",
+        "nextPage": "Página siguiente"
+    },
+    "find": {
+        "header": "Buscar",
+        "searchboxPlaceholder": "Introduce una palabra de búsqueda...",
+        "noresults": "No hemos podido encontrar ningún juego con esta palabra<br />Por favor inténtalo con una diferente!"
+    },
+    "talk": {
+        "header": "Debate",
+        "explaination": "La comunidad sobre Game Builder Garage es enorme! Aquí puedes encontrar otros proyectos lugares para encontrar creadores y aprender más de Game Builder Garage!",
+        "missing": "¿Nos hemos dejado algún proyecto? Comentánoslo en nuestro Discord!"
+    },
+    "channelOverview": {
+        "header": "Descripción del canal",
+        "explaination": "Descubre nuevos juegos por tema específico, estilo o concepto."
+    },
+    "channelDetail": {
+        "snackbar": {
+            "serverError": "El canal no puede cargarse debido a un error del servidor. Por favor, inténtalo más tarde.",
+            "notFound": "No hemos encontrado ningún canal que coincida con el criterio de búsqueda."
+        }
+    },
+    "gameAdd": {
+        "header": "Añadir un nuevo juego",
+        "explaination": "¿Has creado algún juego con Game Builder Garage?<br />Añádelo a MyGarage.games para compartirlo con el mundo!",
+        "rules": {
+            "header": "Reglas de inscripción",
+            "maynot": "Tu inscripción <strong>no debe</strong>",
+            "ruleHateful": "ser odioso, inapropiado, sexista o xenófogo. Esto incluye contenido contyra etnias, sexualidades o minorías.",
+            "ruleIP": "transgreder el copyright o propiedad intelectual de otras entidades.",
+            "ruleExplicit": "contener contenido adulto como desnudos, violencia o cualquier tipo de contenido gráfico afonsivo. Esto se aplica a el contenido del juego, los elementos multimedia y la descripción.",
+            "ruleLowEffort": "No crees múltiples inscripciones para el mismo juego. No inscribas contenido de una manera vaga.",
+            "ruleContentNotice": "Por favor, si tu inscripción posee fuertes componentes gráficos especifícalo para proteger a los que sean sensibles a ellos.",
+            "ruleModeration": "El equipo de moderación ocultará capturas, y borrará o ocultará videos de inscripciones si es necesario. Si violas las reglas de inscripción repetidamente, evitaremos que puedas introducir contenido en el futuro o desactivaremos tu cuenta.",
+            "ruleDiscretion": "Estas reglas no son una lista cerrada. Los moderadores actçúan siempre bajo su propio criterio. Nos reservamos el derecho a ajustar estas y hacer excepciones en cualquier regla si es necesario."
+        },
+        "general": {
+            "header": "Información general",
+            "form": {
+                "title": "Título de tu inserción",
+                "titlePlaceholder": "Mi juego super increíble",
+                "description": "Por favor, describe tu inserción. ¿Qué tipo de experiencia es? ¿Cómo se juega?",
+                "ingameID": "Introduce el ID que ves en Game Builder Garage después de subirlo",
+                "ingameIDPlaceholder": "G-000-000-000"
+            }
+        },
+        "discoverability": {
+            "header": "Visibilidad",
+            "form": {
+                "displayStatus": "¿Quién será capaz de ver tu inserción?",
+                "displayStatus0": "Todos",
+                "displayStatus1": "Oculta - Sólo las personas con el enlace pueden acceder",
+                "displayStatus2": "Privada - Sólo tú puedes acceder a ella",
+                "channels": "Selecciona los canales para tu inserción (Mantén CTRL para seleccionar múltiples canales)",
+
+            }
+        },
+        "media": {
+            "header": "Media adicional",
+            "form": {
+                "coverFile": "Imagen de portada (formato 16:9, extensión .jpg o .png)",
+                "youtubeUrl": "¿Tienes un trailer en YouTube? ¡Añade la dirección aquí!"
+            }
+        },
+        "done": {
+            "success": {
+                "header": "¡Eso es!",
+                "text": "bien hecho, tu juego ha sido publicado correctamente. Ahora puedes añadir capturas, ajustar el tema y compartirlo visitando la página del juego.",
+                "coverWrongFormat": "La imagen de portada no ha podido ser subida porque no es una extensión de archivo válida. Por favor, ten en cuenta que sólo archivos PNG y JPEG son admitidos como imágenes de portada."
+            },
+            "error": {
+                "header": "Ups!",
+                "text": "Hubo un error publoicando tu juego. Por favor inténtalo otra vez más tarde."
+            }
+        },
+        "requiredNotice": "* = obligatorio",
+        "action": {
+            "backButton": "Atrás",
+            "agreeButton": "Estoy deacuerdo",
+            "continueButton": "Continuar",
+            "goToGameButton": "Ir a la página del juego"
+        },
+        "snackbar": {
+            "fillOut": "Tienes que completar todos los campos obligatorios para continuar.",
+            "gameIDInvalid": "Tu ID de juego no es válido (formato: G-000-000-000)."
+        }
+    },
+    "gameDelete": {
+        "header": "Borrar un juego",
+        "explaination": "Todas las acciones son irreversibles, ¡sé cuidadoso!",
+        "confirm": {
+            "header": "¿Estás seguro?",
+            "text": "Estás a punto de borrar tu juego <strong>{gameTitle}</strong> de MyGarage.games. Todas las imágenes y los comentarios serán borrados con él. Esta acción es irreversible.",
+            "nevermindButton": "Olvídalo",
+            "yesButton": "Sí, ¡adelante!"
+        },
+        "snackbar": {
+            "success": "El juego ha sido borrado.",
+            "error": "No se puede borrar el juego."
+        }
+    },
+    "gameDetail": {
+        "action": {
+            "edit": "Editar",
+            "delete": "Borrar"
+        },
+        "comments": {
+            "form": {
+                "notLoggedIn": "Ingresa para escribir comentarios<br />y enviar valoraciones!",
+                "commentButton": "Comentar"
+            },
+            "noComments": "Este juego aún no tiene comentarios."
+        },
+        "trailerOverlay": {
+            "closeButton": "Cerrar"
+        },
+        "snackbar": {
+            "error": {
+                "serverError": "El juego no puede cargarse debido a un error del servidor. Por favor inténtalo más tarde",
+                "notFound": "No hemos encontrado un juego que coincida con los criterios de búsqueda.",
+                "private": "No estas autorizado a ver este juego."
+            },
+            "comments": {
+                "success": {
+                    "posted": "Tu comentario ha sido publicado.",
+                    "deleted": "Tu comentario ha sido borrado."
+                },
+                "error": {
+                    "serverError": "Los comentarios no pueden cargarse debido a un error del servidor. Por favor inténtalo más tarde",
+                    "postedServerError": "El comentario no puede publicarse debido a un error del servidor. Por favor inténtalo más tarde",
+                    "deletedServerError": "El comentario no puede borrarse debido a un error del servidor. Por favor inténtalo más tarde",
+                    "notEmpty": "Los comentarios no pueden estar vacíos."
+                }
+            }
+        }
+    },
+    "gameEdit": {
+        "header": "Editar un juego",
+        "explaination": "Modifica la información, añade capturas o trailers y publica / despublica tu juego.",
+        "media": {
+            "form": {
+                "fileHint": ".png / .jpg",
+                "deleteCoverButton": "Borrar portada"
+            }
+        },
+        "screenshots": {
+            "header": "Capturas"
+        },
+        "backToGamePageButton": "Volver a la página del juego",
+        "saveButton": "Guardar",
+        "snackbar": {
+            "success": {
+                "screenshotDeleted": "La captura ha sido borrada.",
+                "coverUpdated": "Captura actualizada.",
+                "coverDeleted": "Captura borrada.",
+                "screenshotUploaded": "Capturas cargadas correctamente.",
+                "saved": "Guardar cambios."
+            },
+            "error": {
+                "loadServerError": "El juego no puede cargarse debido a un error del servidor. Por favor inténtalo más tarde.",
+                "loadNotFound": "No hemos podido encontrar un juego que coincida con la búsqueda.",
+                "loadPrivate": "No tienes permisos para ver este juego.",
+                "notAllowed": "No tienes permisos para editar este juego.",
+                "screenshotDeleted": "No se ha podido borrar la captura. Por favor inténtalo más tarde.",
+                "coverWrongFormat": "La imagen de portada no ha podido ser actualizada porque no es una extensión de archivo válida. ¡Sólo archivos de imagen PNG y JPEG son admitidos!",
+                "coverServerError": "No se ha podido actualizar la captura. Por favor inténtalo más tarde.",
+                "coverDeleteServerError": "No se ha podido borrar la captura. Por favor inténtalo más tarde.",
+                "screenshotWrongFormat": "Una o más imágenes no se han podido cargar debido a que su extensión de archivo es incorrecta. ¡Sólo archivos de imagen PNG y JPEG son admitidos!",
+                "screenshotUploadServerError": "No se ha podido actualizar la captura debido a un error del servidor. ¡Inténtalo más tarde!",
+                "requiredEmpty": "¡Los compos obligatorios no pueden estar vacíos!",
+                "gameIDInvalid": "El ID de tu juego no es válido (formato: G-000-000-000).",
+            }
+        }
+    },
+    "playlistDetail": {
+        "header": "Lista de reproducción",
+        "nogames": "Aún no hay juegos en esta lista.",
+        "snackbar": {
+            "error": {
+                "notFound": "La lista no se ha podido cargar debido a un problema del servidor. Por favor inténtalo más tarde.",
+                "notAllowed": "No tienes permiso para ver esta lista."
+            }
+        }
+    },
+    "userDetail": {
+        "meta": {
+            "pronouns": "Pronombres",
+            "ingameID": "ID en juego",
+            "joinedDate": "Antiguedad",
+            "teamVerified": "Este usuario es un miembro verificado del equipo de MyGarage.games team."
+        },
+        "action": {
+            "edit": "Editar",
+            "ban": "Banear usuario"
+        },
+        "snackbar": {
+            "notFound": "No se puede encontrar el usuario.",
+            "serverError": "El usuario no se puede mostrar debido a un error del servidor. Por favor inténtalo más tarde.",
+            "banned": "Este usuario ha sido baneado."
+        },
+        "nogames": "Este usuario aún no ha cargado ningún juego."
+    },
+    "userEdit": {
+        "header": "Actializa tu perfil",
+        "explaination": "Añadir un avatar, cambia tu biografía o contraseña.",
+        "tabs": {
+            "general": "General",
+            "avatar": "Avatar",
+            "security": "Securidad",
+            "language": "Idioma"
+        },
+        "form": {
+            "username": "Nombre de usuario",
+            "pronouns": "Pronombres",
+            "ingameID": "ID en juego",
+            "ingameIDPlaceholder": "P-000-000-000",
+            "avatarImage": "Imagen de avatar (formato 1:1, .jpg o .png)",
+            "deleteAvatarButton": "Borrar avatar",
+            "email": "Email",
+            "password": "Contraseña",
+            "passwordAgain": "Contraseña (Repetir)",
+            "language": "Idioma"
+        },
+        "backToProfileButton": "Volver al perfil",
+        "saveButton": "Save",
+        "languageNotice": "Las traducciones son creadas por la comunidad. Únete a nuestro Discord si quieres ayudarnos.",
+        "snackbar": {
+            "success": {
+                "avatarUpdated": "Avatar ha sido actualizado.",
+                "avatarDeleted": "Avatar ha sido borrado.",
+                "saved": "Cambios guardados."
+            },
+            "error": {
+                "notAllowed": "No tienes permisos para editar este usuario.",
+                "avatarWrongFormat": "El avatar no ha podido ser actualizado porque no es una extensión de archivo válida. ¡Sólo archivos de imagen PNG y JPEG son admitidos!",
+                "avatarServerError": "No se ha actualizado el avatar. Por favor inténtalo más tarde.",
+                "avatarDeleteServerError": "No se ha borrado el avatar. Por favor inténtalo más tarde.",
+                "usernameForbidden": "Este nombre de usuario contiene caracteres no admitidos. Los caracteres admitidos son a-z, A-Z, 0-9, guiones y guiones bajos.",
+                "saveServerError": "Tus cambios no se han guardado debido a un error de servidor. Por favor inténtalo más tarde.",
+                "saveIngameIDInvalid": "El Id de tu perfil tiene un formato no válido (formato: P-000-000-000).",
+                "saveUsernameConflict": "¡Este nombre de usuario ya esta en uso!",
+                "securityNotIdentical": "Las contraseñas introducidas no coinciden.",
+                "securityEmailInvalid": "¡Tu email no es válido!",
+                "socialDiscordInvalid": "Tu etiqueta de Discord no es válida. Por favor, facilita una con tu ID (example#0000)."
+            }
+        }
+    },
+    "comment": {
+        "deleteButton": "Borrar"
+    },
+    "game": {
+        "snackbar": {
+            "success": {
+                "addToPlaylist": "{gameTitle} ha sido añadido a tu lista.",
+                "deleteFromPlaylist": "{gameTitle} ha sido borrado de tu lista."
+            },
+            "error": {
+                "addToPlaylistServerError": "Game couldn't be added to your playlist due to a server error. Please try again later",
+                "deleteFromPlaylistServerError": "Game couldn't be deleted from your playlist due to a server error. Please try again later"
+            }
+        }
+    },
+    "footer": {
+        "disclaimer": "MyGarage.games es una plataforma gratuita de la comunidad y no esta afiliada a Nintendo de ninguna manera.<br />Game Builder Garage es una marca de Nintendo.",
+        "legalLink": "Información legal",
+        "apiLink": "API"
+    }
+}
+
+export default messages;
